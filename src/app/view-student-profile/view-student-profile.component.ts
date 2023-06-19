070baa63fe8c1eb866c470527dd485357674b6fe
Reda <sr5678467@gmail.com>
2023-06-19 15:14:10 +0300

    Search done

---
+++ src/app/view-student-profile/view-student-profile.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-view-student-profile',
+  templateUrl: './view-student-profile.component.html',
+  styleUrls: ['./view-student-profile.component.css']
+})
+export class ViewStudentProfileComponent {
+  StudentData:any
+  constructor(private router: Router,private route: ActivatedRoute){}
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.StudentData=params
+      console.log("this.StudentData",this.StudentData)
+     }); 
+  }
+  
+
+}
