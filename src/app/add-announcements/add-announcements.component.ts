06c1a4afb3de2e144ab23ead5eef975d8ddd2cf6
SaraMohamed-20190234 <saramohamed12656@gmail.com>
2023-06-23 22:57:34 +0300

    ss

--- src/app/add-announcements/add-announcements.component.ts
+++ src/app/add-announcements/add-announcements.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
- import tinymce from 'tinymce';
+//  import tinymce from 'tinymce';
 
 @Component({
   selector: 'app-add-announcements',
